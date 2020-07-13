eb58b2a56796431a721f67d408f651f85c80a7df
1880akshay <1880akshay@gmail.com>
2020-07-13 20:36:20 +0530

    mobile edit

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-    
+
     var subTotal = 0;
     for(var i=0; i<$('.cart-prod-total-price').length; i++) {
         var quantity = parseInt($('.cart-prod-quantity:eq('+i+')').val());
         var actualPrice = Number($('.cart-prod-actual-price:eq('+i+')').html());
         subTotal += actualPrice*quantity;
-        $('.cart-prod-total-price:eq('+i+')').html(actualPrice*quantity)
+        $('.cart-prod-total-price:eq('+i+')').html(actualPrice*quantity);
+        $('.cart-prod-total-price-mobile:eq('+i+')').html(actualPrice*quantity);
     }
     var discount = Number($('.cart-discount').html());
     var delivery = Number($('.cart-delivery-charge').html());
@@ -23,6 +24,7 @@ $(document).ready(function() {
             $('.cart-prod-quantity:eq('+index+')').val(++quantity);
             var actualPrice = Number($('.cart-prod-actual-price:eq('+index+')').html());
             $('.cart-prod-total-price:eq('+index+')').html(actualPrice*quantity);
+            $('.cart-prod-total-price-mobile:eq('+index+')').html(actualPrice*quantity);
             subTotal += actualPrice;
             $('.cart-sub-total').html(subTotal);
             $('.cart-final-total').html(subTotal-discount+delivery);
@@ -41,6 +43,7 @@ $(document).ready(function() {
                 $('.cart-prod-quantity:eq('+index+')').val(--quantity);
                 var actualPrice = Number($('.cart-prod-actual-price:eq('+index+')').html());
                 $('.cart-prod-total-price:eq('+index+')').html(actualPrice*quantity);
+                $('.cart-prod-total-price-mobile:eq('+index+')').html(actualPrice*quantity);
                 subTotal -= actualPrice;
                 $('.cart-sub-total').html(subTotal);
                 $('.cart-final-total').html(subTotal-discount+delivery);
